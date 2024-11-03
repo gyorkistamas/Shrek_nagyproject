d95d9e89ce6efbf5b6e73ef4bf89ed8a20685518
Eloreaper <elloreaper@gmail.com>
2024-11-03 17:45:51 +0100

    Revert "Laravel Project Fix"
    This reverts commit 44369693daa37e2ade079bc98489d83fc9079752.

--- autosiskola-app/routes/web.php
+++ autosiskola-app/routes/web.php
@@ -1,7 +1,28 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AngularController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{any}', [AngularController::class,'index'])->where('any', '^(?!api).*$');
+
+
+
+
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
