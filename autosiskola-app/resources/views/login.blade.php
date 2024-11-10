79e0f8e55a7825aebf16c73ebc91ee22e8b5af45
Equestrianmage <equestrianmage@gmail.com>
2024-11-10 13:33:15 +0100

    Login typo fix

--- autosiskola-app/resources/views/login.blade.php
+++ autosiskola-app/resources/views/login.blade.php
@@ -1,3 +1,6 @@
-<div>
-    <!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-</div>
+<!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
+<form action="" method="post">
+    <input type="email" name="email" placeholder="example@placeholder.com">
+    <input type="password" name="password" placeholder="password">
+    <button type="submit">Bejelentkezés</button>
+</form>
