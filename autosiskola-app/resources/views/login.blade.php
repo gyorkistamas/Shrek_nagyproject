0a795f492757fb42d35dc7831d4351425af03dca
Eloreaper <elloreaper@gmail.com>
2024-11-11 09:05:21 +0100

    nemhasznált login file törölve

--- autosiskola-app/resources/views/login.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Live as if you were to die tomorrow. Learn as if you were to live forever. - Mahatma Gandhi -->
-<form action="" method="post">
-    <input type="email" name="email" placeholder="example@placeholder.com">
-    <input type="password" name="password" placeholder="password">
-    <button type="submit">Bejelentkezés</button>
-</form>
