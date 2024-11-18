4452a854887676e66fefaeaff0ae7cb3b8ae1e1c
macskalacska666 <tb2003723@gmail.com>
2024-11-18 08:38:04 +0100

    Bejelentkezés után a Dashboardon ne látszódjon a Regisztráció/Bejelentkezés lehetőség

--- autosiskola-app/resources/views/layouts/navigation.blade.php
+++ autosiskola-app/resources/views/layouts/navigation.blade.php
@@ -5,70 +5,64 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-        <!-- Főoldal hivatkozás -->
-        <a href="{{ url('/') }}" style="margin-right: 10px;" >
-            <!---<x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />--->
-            <img src="{{ asset('images/auto.png') }}" alt="Logo">
-        </a>
-    </div>
+                    <a href="{{ url('/') }}" style="margin-right: 10px;">
+                        <img src="{{ asset('images/auto.png') }}" alt="Logo">
+                    </a>
+                </div>
 
-            <!-- Navigation Links
-            <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                Dashboard hivatkozás
-                <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                    {{ __('Dashboard') }}
-                </x-nav-link>
+                <!-- Navigation Links -->
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <!-- Ha a felhasználó nincs bejelentkezve, jelenjen meg a regisztrációs link -->
+                    @guest
+                        <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                            {{ __('Regisztráció') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                            {{ __('Bejelentkezés') }}
+                        </x-nav-link>
+                    @endguest
 
-            </div>-->
-
-             <!-- Regisztráció Link -->
-                <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                    {{ __('Regisztráció') }}
-                </x-nav-link>
+                    <!-- Ha a felhasználó be van jelentkezve, jelenjen meg a profil és kijelentkezés link -->
+                    @auth
+                        <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                            {{ __('Dashboard') }}
+                        </x-nav-link>
+                    @endauth
+                </div>
             </div>
-
-            <!-- Bejelentkezés Link -->
-            <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                    {{ __('Bejelentkezés') }}
-                </x-nav-link>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
-                <x-dropdown align="right" width="48">
-                    <x-slot name="trigger">
-                        <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
-                        <div>
-                            @if (Auth::check())
-                                {{ Auth::user()->name }}
-                            @else
-                                Vendég
-                            @endif
-                        </div>
-                            <div class="ms-1">
-                                <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
-                                </svg>
-                            </div>
-                        </button>
-                    </x-slot>
+                @auth
+                    <x-dropdown align="right" width="48">
+                        <x-slot name="trigger">
+                            <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                                <div>{{ Auth::user()->name }}</div>
+                                <div class="ms-1">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                    </svg>
+                                </div>
+                            </button>
+                        </x-slot>
 
-                    <x-slot name="content">
-                        <x-dropdown-link :href="route('profile.edit')">
-                            {{ __('Profil') }}
-                        </x-dropdown-link>
-
-                        <!-- Authentication -->
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-
-                            <x-dropdown-link :href="route('logout')"
-                                    onclick="event.preventDefault();
-                                                this.closest('form').submit();">
-                                {{ __('Kijelentkezés') }}
+                        <x-slot name="content">
+                            <x-dropdown-link :href="route('profile.edit')">
+                                {{ __('Profil') }}
                             </x-dropdown-link>
-                        </form>
-                    </x-slot>
-                </x-dropdown>
+
+                            <!-- Authentication -->
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <x-dropdown-link :href="route('logout')"
+                                        onclick="event.preventDefault();
+                                                    this.closest('form').submit();">
+                                    {{ __('Kijelentkezés') }}
+                                </x-dropdown-link>
+                            </form>
+                        </x-slot>
+                    </x-dropdown>
+                @endauth
             </div>
 
             <!-- Hamburger -->
@@ -85,54 +79,56 @@
 
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
-    <div class="pt-2 pb-3 space-y-1">
-        <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-            {{ __('Dashboard') }}
-        </x-responsive-nav-link>
+        <div class="pt-2 pb-3 space-y-1">
+            @guest
+                <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
+                    {{ __('Regisztráció') }}
+                </x-responsive-nav-link>
+                <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
+                    {{ __('Bejelentkezés') }}
+                </x-responsive-nav-link>
+            @endguest
 
-        <!-- Regisztráció Link -->
-        <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
-            {{ __('Regisztráció') }}
-        </x-responsive-nav-link>
-
-        <!-- Bejelentkezés Link -->
-        <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
-                {{ __('Bejelentkezés') }}
-            </x-responsive-nav-link>
-    </div>
+            @auth
+                <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
+                    {{ __('Dashboard') }}
+                </x-responsive-nav-link>
+            @endauth
+        </div>
 
         <!-- Responsive Settings Options -->
         <div class="pt-4 pb-1 border-t border-gray-200 dark:border-gray-600">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800 dark:text-gray-200">
-                            @if (Auth::check())
-                                {{ Auth::user()->name }}
-                            @else
-                                Vendég
-                            @endif</div>
+                    @if (Auth::check())
+                        {{ Auth::user()->name }}
+                    @else
+                        Vendég
+                    @endif
+                </div>
                 <div class="font-medium text-sm text-gray-500">
-                            @if (Auth::check())
-                                {{ Auth::user()->email }}
-                            @else
-                                Vendég
-                            @endif</div>
+                    @if (Auth::check())
+                        {{ Auth::user()->email }}
+                    @else
+                        Vendég
+                    @endif
+                </div>
             </div>
 
             <div class="mt-3 space-y-1">
-                <x-responsive-nav-link :href="route('profile.edit')">
-                    {{ __('Profil') }}
-                </x-responsive-nav-link>
-
-                <!-- Authentication -->
-                <form method="POST" action="{{ route('logout') }}">
-                    @csrf
-
-                    <!-- A gomb kattintásakor az esemény megakadályozza az alapértelmezett link navigációját és a formot küldi -->
-                    <x-responsive-nav-link href="#"
-                        onclick="event.preventDefault(); this.closest('form').submit();">
-                        {{ __('Kijelentkezés') }}
+                @auth
+                    <x-responsive-nav-link :href="route('profile.edit')">
+                        {{ __('Profil') }}
                     </x-responsive-nav-link>
-                </form>
+
+                    <form method="POST" action="{{ route('logout') }}">
+                        @csrf
+                        <x-responsive-nav-link href="#"
+                            onclick="event.preventDefault(); this.closest('form').submit();">
+                            {{ __('Kijelentkezés') }}
+                        </x-responsive-nav-link>
+                    </form>
+                @endauth
             </div>
         </div>
     </div>
