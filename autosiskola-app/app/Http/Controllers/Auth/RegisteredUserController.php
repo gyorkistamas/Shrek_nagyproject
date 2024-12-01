6b3207b5d4592e40a3ddf1da16567eecca471991
Equestrianmage <equestrianmage@gmail.com>
2024-12-01 14:18:53 +0100

    Mostmár mindent bekér és eltárolja jó helyen

--- autosiskola-app/app/Http/Controllers/Auth/RegisteredUserController.php
+++ autosiskola-app/app/Http/Controllers/Auth/RegisteredUserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\felhasznalo;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
@@ -23,6 +24,14 @@ class RegisteredUserController extends Controller
         $request->validate([
             'email' => ['required', 'string', 'email', 'max:200', 'unique:bejelentkezes,email'],
             'password' => ['required', 'string', 'min:3', 'max:100', 'confirmed'],
+            'nev' => ['required', 'string', 'unique:felhasznalo,nev'],
+            'taj' => ['required', 'string', 'min:9','max:9'],
+            'szemelyi' => ['required', 'string', 'min:8','max:8'],
+            'adoszam' => ['required', 'string', 'min:10','max:10'],
+            'szulido' => ['required', 'date'],
+            'szulhely' => ['required', 'string'],
+            'elsosegelyvizsga' => ['nullable','boolean'],
+            'szemuveg' => ['nullable','boolean'],
         ]);
 
 
@@ -35,6 +44,17 @@ class RegisteredUserController extends Controller
             'felhasznalo' => $randomId,
             'email' => $request->email,
             'jelszo' => Hash::make($request->password),
+        ]);
+        $felhasznalo = felhasznalo::create([
+            'id' => $randomId,
+            'nev' => $request->nev,
+            'taj' => $request->taj,
+            'szemelyi' => $request->szemelyi,
+            'adoszam' => $request->adoszam,
+            'szulido' => $request->szulido,
+            'szulhely' => $request->szulhely,
+            'elsosegelyvizsga' => $request->elsosegelyvizsga,
+            'szemuveg' => $request->szemuveg,
         ]);
 
 
