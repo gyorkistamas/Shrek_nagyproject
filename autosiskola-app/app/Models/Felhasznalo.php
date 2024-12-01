f5cd1146a36597f751dc67db3c0b54990e7423b5
Equestrianmage <equestrianmage@gmail.com>
2024-12-01 14:19:12 +0100

    Modell hogy mentsen a felhasználó táblába

---
+++ autosiskola-app/app/Models/Felhasznalo.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class felhasznalo extends Model
+{
+    public $timestamps = false;
+    protected $table = 'felhasznalo';
+    protected $primaryKey = 'taj';
+    public $incrementing = false;
+    protected $keyType = 'int';
+
+    protected $fillable = [
+        'id',
+        'nev',
+        'taj',
+        'szemelyi',
+        'adoszam',
+        'szulido',
+        'szulhely',
+        'elsosegelyvizsga',
+        'szemuveg',
+    ];
+
+    protected $hidden = [
+
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'id' => 'int',
+            'nev' => 'string',
+            'taj' => 'int',
+            'szemelyi' => 'int',
+            'adoszam' => 'int',
+            'szulido' => 'datetime',
+            'szulhely' => 'string',
+            'elsosegelyvizsga' => 'boolean',
+            'szemuveg' => 'boolean',
+        ];
+    }
+}
