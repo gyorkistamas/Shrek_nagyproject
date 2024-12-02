633c30f83262b17f8dc3ab99b6be39c518d6aa88
Equestrianmage <equestrianmage@gmail.com>
2024-12-02 16:45:47 +0100

    Jó táblát használ már

--- autosiskola-app/app/Http/Controllers/editController.php
+++ autosiskola-app/app/Http/Controllers/editController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Felhasznalo;
+use Auth;
 use Illuminate\Http\Request;
 
 class editController extends Controller
 {
     public function edit()
-    {
-        return view('edit');
-    }
+{
+    // Example: Fetch Felhasznalo by ID (not by taj)
+    $felhasznalo = Felhasznalo::where('id', Auth::id())->first();
+
+    return view('edit', compact('felhasznalo'));
+}
 }
