9dc4594608cade2bc51967be71df8841bbeba807
Equestrianmage <equestrianmage@gmail.com>
2024-12-02 16:38:09 +0100

    Dashboard már jó adatokat jelenít meg

--- autosiskola-app/app/Http/Controllers/DashboardController.php
+++ autosiskola-app/app/Http/Controllers/DashboardController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Felhasznalo;
+use Auth;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index()
-    {
-        return view('dashboard');
-    }
+{
+    // Example: Fetch Felhasznalo by ID (not by taj)
+    $felhasznalo = Felhasznalo::where('id', Auth::id())->first();
+
+    return view('dashboard', compact('felhasznalo'));
+}
 }
