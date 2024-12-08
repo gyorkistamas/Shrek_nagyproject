1e5e2bb64181f91e4254b92c2aef513122a2dd4a
Eloreaper <elloreaper@gmail.com>
2024-12-08 09:35:21 +0100

    Minden "felhasznalo" tábla adat szerkeszthető

--- autosiskola-app/resources/views/users/edit.blade.php
+++ autosiskola-app/resources/views/users/edit.blade.php
@@ -23,6 +23,37 @@
                 <input type="date" class="form-control" id="szulido" name="szulido" value="{{ $user->szulido->format('Y-m-d') }}" required>
             </div>
 
+            <div class="form-group">
+                <label for="taj">TAJ szám:</label>
+                <input type="text" class="form-control" id="taj" name="taj" value="{{ $user->taj }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="adoszam">Adószám:</label>
+                <input type="text" class="form-control" id="adoszam" name="adoszam" value="{{ $user->adoszam }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="szulhely">Születési hely:</label>
+                <input type="text" class="form-control" id="szulhely" name="szulhely" value="{{ $user->szulhely }}" required>
+            </div>
+
+            <div class="form-group">
+                <label for="elsosegelyvizsga">Elsősegély vizsga:</label>
+                <select class="form-control" id="elsosegelyvizsga" name="elsosegelyvizsga" required>
+                    <option value="1" {{ $user->elsosegelyvizsga ? 'selected' : '' }}>Igen</option>
+                    <option value="0" {{ !$user->elsosegelyvizsga ? 'selected' : '' }}>Nem</option>
+                </select>
+            </div>
+
+            <div class="form-group">
+                <label for="szemuveg">Szemüveg szükséges:</label>
+                <select class="form-control" id="szemuveg" name="szemuveg" required>
+                    <option value="1" {{ $user->szemuveg ? 'selected' : '' }}>Igen</option>
+                    <option value="0" {{ !$user->szemuveg ? 'selected' : '' }}>Nem</option>
+                </select>
+            </div>
+
             <button type="submit" class="btn btn-success">Frissítés</button>
         </form>
     </div>
