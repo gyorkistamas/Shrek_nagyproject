f6b749747f5309899a2a7708426ae1b3572bfd55
Eloreaper <elloreaper@gmail.com>
2024-12-08 13:08:52 +0100

    Óra index nézett

---
+++ autosiskola-app/resources/views/orak/index.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Vizsga időpontok</h1>
+    <a href="{{ route('orak.create') }}" class="btn btn-primary">Új vizsga időpont</a>
+    <table class="table mt-4">
+        <thead>
+            <tr>
+                <th>Időpont</th>
+                <th>Időtartam (perc)</th>
+                <th>Oktató</th>
+                <th>Diák</th>
+                <th>Műveletek</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($orak as $ora)
+                <tr>
+                    <td>{{ $ora->datum }}</td>
+                    <td>{{ $ora->idotartam_perc }}</td>
+                    <td>{{ $ora->oktato }}</td>
+                    <td>{{ $ora->diak }}</td>
+                    <td>
+                        <a href="{{ route('orak.edit', $ora->oraID) }}" class="btn btn-warning">Szerkesztés</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
